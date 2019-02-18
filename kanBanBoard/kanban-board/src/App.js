ccad3a8a13fcc50f7cebb25f9b13349a8b2fc37b
davisa94 <austin.i.davis94@gmail.com>
2019-02-18 13:08:13 -0700

    added a few things to help with styling the columns

--- kanBanBoard/kanban-board/src/App.js
+++ kanBanBoard/kanban-board/src/App.js
@@ -2,24 +2,94 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+class Card extends Component{
+  text = "default text";
+  leftButton = "L";
+  rightButton = "R";
+  render(){
+    return (
+      <div className="card">
+        {this.props.leftButton} {this.props.text} {this.props.rightButton}
+      </div>
+
+    )
+  } 
+
+}
+
+
+var cards = [
+  {
+   leftButton: '<button class = "card-button" type="button" name="button"><</button>',
+   text: 'First Card',
+   rightButton: 'R'
+  },
+  {
+    leftButton: 'L',
+    text: 'Second Card',
+    rightButton: 'R'
+   },
+   {
+    leftButton: 'L',
+    text: 'Third Card',
+    rightButton: 'R'
+   },
+   {
+    leftButton: 'L',
+    text: 'Fourth Card',
+    rightButton: 'R'
+   },
+     
+];
+
+class Board extends Component {
+  constructor(classN="column"){
+    var classname = classN;
+  }
+
+  render(){
+    const listItems = cards.map((card)=>
+    <div className="card" key={card.text}>{card.text}</div>);
+    return(
+      <div className="column">
+        {listItems}
+      </div>
+    )
+  }
+}
+
+var boards =
+[
+  {
+    board: new Board("column green")
+  },
+  {
+    board: new Board("column blue")
+  }
+]
+
+
+class mainBoard extends Component {
+
+  render(){
+    const listItems = boards.map((board)=>
+    <div className={board.props.classname}></div>);
+    return(
+      <div className="main">
+        {listItems}
+      </div>
+    )
+  }
+
+}
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="main">
+        <mainBoard></mainBoard>
+        </div>
       </div>
     );
   }
